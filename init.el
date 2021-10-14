f6ddb95e2d3f6ddfd67e6ba036d61fd53088d8d8
Sunny Hasija <hasija.4@osu.edu>
2021-10-14 13:42:50 -0400

    Refactor,Config 2.0

--- init.el
+++ init.el
@@ -4,47 +4,50 @@
 ;; in. Remember to run 'doom sync' after modifying it!
 
 ;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
-;;      documentation. There you'll find information about all of Doom's
-;;      modules and what flags they support.
+;;      documentation. There you'll find a "Module Index" link where you'll find
+;;      a comprehensive list of Doom's modules and what flags they support.
 
 ;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
-;;      'C-c g k' for non-vim users) to view its documentation. This works on
+;;      'C-c c k' for non-vim users) to view its documentation. This works on
 ;;      flags as well (those symbols that start with a plus).
 ;;
-;;      Alternatively, press 'gd' (or 'C-c g d') on a module to browse its
+;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
 ;;      directory (for easy access to its source code).
-
 (doom! :input
        ;;chinese
        ;;japanese
+       ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
        company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +icons +prescient)               ; a search engine for love and life
+       (ivy +icons +fuzzy +posframe)               ; a search engine for love and life
+     ; (vertico +icons)
 
        :ui
        deft              ; notational velocity for Emacs
        doom              ; what makes DOOM look the way it does
        doom-dashboard    ; a nifty splash screen for Emacs
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
+       (emoji +unicode)  ; 🙂
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
-      ;; indent-guides     ; highlighted indent columns
+       hydra
+       ;;indent-guides     ; highlighted indent columns
+       (ligatures +extra )         ; ligatures and symbols to make your code pretty again
+       minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
+       nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ligatures       ; ligatures or substitute text with pretty symbols
-       tabs              ; an tab bar for Emacs
-       treemacs          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       tabs              ; a tab bar for Emacs
+       (treemacs +lsp)          ; a project drawer, like neotree but cooler
+       unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
-       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       (window-select +numbers)     ; visually switch windows
+       ;vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
 
@@ -58,31 +61,32 @@
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
-       rotate-text       ; cycle region at point between text candidates
+       ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
-       word-wrap         ; soft wrapping with language-aware indent
+       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        (dired +icons +ranger)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
-       (undo +tree)              ; persistent, smarter undo for your inevitable mistakes
+       undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
-       term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       ;;term              ; basic terminal emulator for Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       spell             ; tasing you for misspelling mispelling
+       (spell +flyspell) ; tasing you for misspelling mispelling
        grammar           ; tasing grammar mistake every you make
+
 
        :tools
        ;;ansible
-       ;;debugger          ; FIXME stepping through code, to help you add bugs
+       debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
@@ -90,17 +94,21 @@
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
-       ;;lsp
-       ;;macos             ; MacOS-specific commands
+       lsp
        magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       pdf               ; pdf enhancements
+       ;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       rgb               ; creating color strings
+       ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
+
+       :os
+       (:if IS-MAC macos)  ; improve compatibility with macOS
+       tty               ; improve the terminal Emacs experience
 
        :lang
        ;;agda              ; types of types of types of types...
@@ -110,43 +118,43 @@
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
-       data              ; config/data formats
+       ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
        emacs-lisp        ; drown in parentheses
        ;;erlang            ; an elegant language for a more civilized age
-       ess               ; emacs speaks statistics
+       ;;ess               ; emacs speaks statistics
        ;;faust             ; dsp, but you get to keep your soul
-       ;;fsharp           ; ML stands for Microsoft's Language
+       ;;fsharp            ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
        ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
-       ;;idris             ;
-       ;;json              ; At least it ain't XML
+       ;;idris             ; a language you can depend on
+       (json +lsp)              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       latex             ; writing papers in Emacs has never been so fun
+       (latex +lsp +fold) ; writing papers in Emacs has never been so fun
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
        ;;lua               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
+       (markdown +grip)          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +roam +pretty +journal +dragndrop +pandoc +hugo)    ; organize your plain life in plain text
-
-       ;;perl              ; write code no one else can comprehend
+       (org +pretty +pomodoro +dragndrop +present +hugo +pandoc +journal +gnuplot +roam2)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-      python            ; beautiful is better than ugly
+       (python +lsp)            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
+       ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
@@ -158,23 +166,21 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
-       ;;yaml              ; JSON, but readable
+       (web +lsp)               ; the tubes
+       yaml              ; JSON, but readable
 
        :email
-       ;;(mu4e +gmail)
+       (mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
        :app
        calendar
+       ;;everywhere        ; *leave* Emacs!? You must be joking
        ;;irc               ; how neckbeards socialize
-       (rss +org)        ; emacs as an RSS reader
+       ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
-       ;;literate
+       literate
        (default +bindings +smartparens))
-;(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-(setq comp-async-env-modifier-form "") ; running GCCemacs.
-
--- packages.el
+++ packages.el
@@ -1,25 +1,16 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-;; To install a package with Doom you must declare them here, run 'doom sync' on
-;; the command line, then restart Emacs for the changes to take effect.
-;; Alternatively, use M-x doom/reload.
-
-
-;; Doom's packages are pinned to a specific commit and updated from release to
-;; release. The `unpin!' macro allows you to unpin single packages...
-;(unpin! pinned-package)
-;; ...or multiple packages
-;(unpin! pinned-package another-pinned-package)
-;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
-;(unpin! t)
+;; To install a package with Doom you must declare them here and run 'doom sync'
+;; on the command line, then restart Emacs for the changes to take effect -- or
+;; use 'M-x doom/reload'.
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
-;; To install a package directly from a particular repo, you'll need to specify
-;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
+;; To install a package directly from a remote git repo, you must specify a
+;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
@@ -31,8 +22,8 @@
 ;  :recipe (:host github :repo "username/repo"
 ;           :files ("some-file.el" "src/lisp/*.el")))
 
-;; If you'd like to disable a package included with Doom, for whatever reason,
-;; you can do so here with the `:disable' property:
+;; If you'd like to disable a package included with Doom, you can do so here
+;; with the `:disable' property:
 ;(package! builtin-package :disable t)
 
 ;; You can override the recipe of a built in package without having to specify
@@ -46,30 +37,130 @@
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
 
-(package! org-ref)
-;(package! interleave)                                        not using anymore as org-noter is far superior.
-(package! helm-bibtex)
-(package! zotxt)
-(package! ox-pandoc)
-(package! org-roam-bibtex)
-(package! org-noter)
-(package! org-roam-server :recipe (:host github :repo "org-roam/org-roam-server" :files ("*")))
+;; Use `:pin' to specify a particular commit to install.
+;(package! builtin-package :pin "1a2b3c4d5e")
+
+
+;; Doom's packages are pinned to a specific commit and updated from release to
+;; release. The `unpin!' macro allows you to unpin single packages...
+;(unpin! pinned-package)
+;; ...or multiple packages
+;(unpin! pinned-package another-pinned-package)
+;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
+;(unpin!;; When using org-roam via the `+roam` flag
 (unpin! org-roam)
-(package! org-download)
-;(package! nov)                                             not using anymore as epub note taking is annoying in nov.el
-(package! rotate)
-;(package! xkcd)
-(package! keycast)
+
+;; When using bibtex-completion via the `biblio` module
+(unpin! bibtex-completion helm-bibtex ivy-bibtex)
+
+(package! evil-workman-mode
+  :recipe (
+           :host github
+           :repo "chrisbarrett/evil-workman-mode"
+           ))
+
+(package! org-roam :recipe (:host github :repo "org-roam/org-roam" ))
+;(package! org-roam-server
+;  :recipe (:local-repo "~/Projects/org-roam-server"
+;           :files ("*")
+;           :build (:not compile)))
+
+;(package! org-roam-server)
+(package! ivy-bibtex)
+(package! org-ref)
+(package! org-roam-bibtex)
 (package! org-super-agenda)
-(package! doct
-  :recipe (:host github :repo "progfolio/doct"))
-(package! org-pretty-table-mode
-  :recipe (:host github :repo "Fuco1/org-pretty-table"))
-(package! org-pretty-tags)
-;(package! centaur-tabs)
-(package! company-org-roam :recipe (:host github :repo "org-roam/company-org-roam"))
-(package! org-fancy-priorities)
-(package! origami)
-;(package! org-journal)     ;DOOM has org-journal built in, enable using +journal flag in init.el
-(package! info-colors ) ; pretty colors
-(package! beacon) ; global minor mode for a blinking highliter to find where the cursor is.
+(package! org-gtd)
+(package! org-noter)
+
+(package! rainbow-mode)
+(package! org-transclusion :recipe (:host github :repo "/nobiot/org-transclusion"
+                                    ;:build (:not compile)
+                                    ))
+(package! org-marginalia :recipe (:host github :repo "nobiot/org-marginalia"))
+
+;(package! nroam)
+(package! org-fragtog)
+(package! org-appear :recipe (:host github :repo "awth13/org-appear"))
+(package! org-noter-pdftools)
+(package! org-pdftools)
+(package! olivetti)
+(package! binder)
+
+(package! focus)
+(package! wc-goal-mode)
+(package! centered-window)
+(package! mixed-pitch)
+(package! wwg :recipe (:host github :repo "ag91/writer-word-goals"))
+(package! powerthesaurus)
+
+(package! tree-sitter)
+(package! tree-sitter-langs)
+
+(package! org-ol-tree
+  :recipe (
+           :host github
+           :repo "Townk/org-ol-tree"
+           ))
+(package! org-sidebar)
+
+;(package! engrave-faces
+ ; :recipe (:host github :repo "tecosaur/engrave-faces"))
+(package! mlscroll)
+
+(package! doom-modeline )
+
+
+(package! erefactor)
+
+(package! svg-tag-mode :recipe (:host github :repo "rougier/svg-tag-mode"))
+(package! mu4e-dashboard :recipe (:host github :repo "rougier/mu4e-dashboard"))
+(package! mu4e-thread-folding :recipe (:host github :repo "rougier/mu4e-thread-folding"))
+;(package! org-agenda :recipe (:host github :repo "rougier/mu4e-thread-folding"))
+
+(package! dashboard)
+;(package! pulse-cursor :recipe (:host nil :repo  https://gist.github.com/jasonjckn/0e92c55f1ab0be0947bb613a4706bc33 ))
+(package! smooth-scrolling)
+(package! good-scroll)
+(package! scroll-on-jump)
+(package! iscroll :recipe (:host github :repo "casouri/iscroll"))
+;(package! yascroll)
+
+(package! ctable)
+(package! deferred)
+(package! s)
+
+(package! info-colors)
+
+(package! org-notifications)
+(package! org-wild-notifier)
+
+;(package! avy)
+(package! visual-regexp)
+(package! visual-regexp-steroids)
+
+(package! font-lock-studio)
+(package! face-explorer)
+
+(package! suggest)
+(package! unpackaged :recipe (:host github :repo "alphapapa/unpackaged.el"))
+
+(package! all-the-icons-ivy-rich)
+
+;(package! page-break-mode :recipe (:local-repo "~/Projects/emacs/page-break-mode"))
+
+(package! websocket)
+(package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
+(package! devdocs)
+(package! prettier)
+
+(package! eva
+    :recipe (:host github :repo "meedstrom/eva"
+           :files (:defaults "assets" "renv" "*.R" "*.gnuplot")))
+
+(package! ivy-posframe)
+
+
+(package! nano-theme :recipe (:host github :repo "rougier/nano-theme"))
+
+(package! org-roam-timestamps :recipe (:host github :repo "ThomasFKJorna/org-roam-timestamps"))
